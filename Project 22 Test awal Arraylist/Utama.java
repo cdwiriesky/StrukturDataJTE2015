a5cf48bc8276075be2dd7f547b58c352155056c4
dwi riesky chandra wiradhika <cdwiriesky@gmail.com>
2015-12-08 14:52:12 +0700

    24 prktiu link list

---
+++ Project 22 Test awal Arraylist/Utama.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+
+public class Utama
+{
+public static void main (String [] args){ 
+
+Scanner input = new Scanner (System.in);
+System.out.println("Bilangan :");
+
+String bilangan = input.nextLine();
+System.out.println("isi : "+bilangan);
+}
+}
